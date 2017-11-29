214e80be8c37949e5aef546a9115db785373b913
Nikkura <linda_badinski@web.de>
2017-11-29 22:11:08 +0100

    Picture Reader
    der Picture Reader liest die rot, blau und grün Werte eines Bildes ein
    scheint zumindest für das Beispielbild, was ich genutzt hab aber keine Benfordreihe zu sein
    vielleicht nochmal mit nur einem Kanal oder dem Alphakanal probieren :)

---
+++ src/PicReader.java
@@ -0,0 +1,44 @@
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Vector;
+
+import javax.imageio.ImageIO;
+
+public class PicReader extends AbstractFileReader {
+	private BufferedImage img;
+	
+	public PicReader(String pathToFile) {
+		super(pathToFile);
+		
+		try {
+		    img = ImageIO.read(getClass().getResource(pathToFile));
+		} 
+		catch (IOException e) {
+		}
+	}
+	
+	public BufferedImage getImg() {
+		return img;
+	}
+	
+	@Override
+	public Vector<String> read() {
+		Vector<String> result = new Vector<>();
+		
+		for (int y = 0; y < img.getHeight(); y++) {
+		    for (int x = 0; x < img.getWidth(); x++) {
+		    	Color cur = new Color(img.getRGB(x, y));
+		    	int r = cur.getRed();
+		    	int b = cur.getBlue();
+		    	int g = cur.getGreen();
+		    	
+		         result.add(""+r);
+		         result.add(""+b);
+		         result.add(""+g);
+		    }
+		}
+		return result;
+	} 
+}
