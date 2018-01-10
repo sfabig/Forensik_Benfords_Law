f46bca49905f266f8087260d827c9639842ff033
Romeo Kamgo <romeo_kamgo@yahoo.com>
2018-01-10 23:43:08 +0100

    gui bearbeitung

---
+++ src/logic/FileTypeFilter.java
@@ -0,0 +1,35 @@
+package logic;
+
+import java.io.File;
+import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
+
+public class FileTypeFilter extends FileFilter {
+	
+	private final String extension;
+	private final String description;
+	
+	public FileTypeFilter(String extension, String description) {
+		this.extension = extension;
+		this.description = description;
+	}
+	
+	public  String getExtension() {
+		return this.extension;
+	}
+	
+	@Override
+	public boolean accept(File file) {
+		if (file.isDirectory()) {
+			return true;
+		}
+		return file.getName().endsWith(extension);
+	}
+	
+	@Override
+	public String  getDescription() {
+		return description + String.format(" (*%s)", extension);
+	}
+	
+
+}
